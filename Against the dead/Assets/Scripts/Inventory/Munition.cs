1b695f721e8b407d51169548431c8b268d5f0d82
Noah Civilise <noah.civilise@epita.fr>
2023-04-10 22:05:57 +0200

    tiens ma louloute

--- Against the dead/Assets/Scripts/Inventory/Munition.cs
+++ Against the dead/Assets/Scripts/Inventory/Munition.cs
@@ -6,16 +6,6 @@ using UnityEngine;
 [Serializable]
 public class Ammunition : Item
 {
-    public enum Caliber
-    {
-        b9, // 9mm (Pistol)
-        b556, // 5,56 (Saug...)
-        b762, // 7,62 (Ak)
-        b12g, // 12 gauge (Shotgun)
-        mele // Fireaxe
-    }
-
-    public int damage { get; }
-    public Caliber caliber { get; }
-    
+    public int damage;
+    public int range;
 }
