a8200f249256444ba1e117327bc884b676594727
Noah Civilise <noah.civilise@epita.fr>
2023-04-11 15:06:27 +0200

    added all weapons and ammunitions for my bebou Martin

---
+++ Against the dead/Assets/Scripts/Inventory/Ammunition.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New ammunition", menuName = "Create new ammo")]
+[Serializable]
+public class Ammunition : Item
+{
+    public int damage;
+    public int range;
+}
